54962d658b5004d639dc5acf54149e221e2b8aa0
Ivan Fernandez <ivan.fernandez@tyris-software.com>
2022-08-02 09:20:59 +0200

    chore: set up Swagger, class validators...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Mini Bank')
+    .setDescription('Bank API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // TODO: create AppConfigService and set PORT in .env file
   await app.listen(3000);
+  console.log(`Application is running on: 3000`);
 }
 bootstrap();
