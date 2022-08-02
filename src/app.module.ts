3edf2ee01faaf0d83c64c2ff392db94480035cd5
Ivan Fernandez <ivan.fernandez@tyris-software.com>
2022-08-02 13:16:32 +0200

    chore: setup typeorm. update package.json scripts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './configuration/app-config.module';
+import { AppConfigService } from './configuration/app-config.service';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [
+    AppConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      useFactory: async (appConfigService: AppConfigService) => ({
+        type: 'postgres',
+        host: appConfigService.getDBHost(),
+        port: appConfigService.getDBPort(),
+        database: appConfigService.getDBName(),
+        username: appConfigService.getDBUsername(),
+        password: appConfigService.getDBPassword(),
+        autoLoadEntities: true,
+        synchronize: appConfigService.getDBSynchronize(),
+      }),
+      inject: [AppConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
