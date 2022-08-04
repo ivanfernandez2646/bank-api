092ab8a47aef863d132cc4aa5d0d1a9493b5b65c
Ivan Fernandez <ivan.fernandez@tyris-software.com>
2022-08-04 10:16:58 +0200

    test: integration e2e

---
+++ test/integration/integration.e2e-spec.ts
@@ -0,0 +1,94 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { getRepository, Repository } from 'typeorm';
+import { AccountModule } from '../../src/entities-manager/account/account.module';
+import { Account } from '../../src/entities-manager/account/entities/account.entity';
+import { CreateOperationDto } from '../../src/operation/dto/create-operation.dto';
+import { Operation } from '../../src/operation/entities/operation.entity';
+import { OperationModule } from '../../src/operation/operation.module';
+import { Utils } from '../utils/utils';
+
+describe('Integration (e2e)', () => {
+  let app: INestApplication;
+  let accountRepository: Repository<Account>;
+  let operationRepository: Repository<Operation>;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AccountModule, OperationModule, Utils.getDBDynamicModule()],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await Utils.initializeAppE2ETest(app);
+
+    accountRepository = getRepository(Account);
+    operationRepository = getRepository(Operation);
+  });
+
+  afterAll(async () => {
+    await Utils.closeAppE2ETest(app);
+  });
+
+  it('create an account, then add few operations and see the operations are in the account', async () => {
+    // 1. create account
+    const account = await createAccount();
+
+    // 2. create three operations (deposit 10, withdraw 2, error on withdraw)
+    const operations = await createOperations(account.id);
+    expect(operations.length).toBe(2);
+
+    // 3. get again the account to see his current balance is equal to 8
+    const res = await request(app.getHttpServer()).get(
+      `/account/${account.id}`,
+    );
+    expect(res.status).toBe(HttpStatus.OK);
+    const accountWithUpdatedBalance: Account = res.body;
+    expect(accountWithUpdatedBalance.id).toBe(account.id);
+    expect(accountWithUpdatedBalance.operations.length).toBe(2);
+    expect(accountWithUpdatedBalance.currentBalance).toBe(8);
+  });
+
+  async function createAccount(): Promise<Account> {
+    const res = await request(app.getHttpServer()).get('/account/random');
+    expect(res.status).toBe(HttpStatus.OK);
+    const account: Account = res.body;
+    const accountBBDD = await accountRepository.findOne({ id: account.id });
+    expect(accountBBDD).toMatchObject(account);
+    return account;
+  }
+
+  async function createOperations(accountId: string): Promise<Operation[]> {
+    const operations: Operation[] = [];
+    const depositDto: CreateOperationDto = {
+      accountId,
+      amount: 10,
+    };
+    let res = await request(app.getHttpServer())
+      .post('/operation/deposit')
+      .send(depositDto);
+    expect(res.status).toBe(HttpStatus.CREATED);
+    operations.push(res.body);
+
+    const withdrawDto: CreateOperationDto = {
+      accountId,
+      amount: 2,
+    };
+    res = await request(app.getHttpServer())
+      .post('/operation/withdraw')
+      .send(withdrawDto);
+    expect(res.status).toBe(HttpStatus.CREATED);
+    operations.push(res.body);
+
+    const errorWithdrawDto: CreateOperationDto = {
+      accountId,
+      amount: 25,
+    };
+    res = await request(app.getHttpServer())
+      .post('/operation/withdraw')
+      .send(errorWithdrawDto);
+    expect(res.status).toBe(HttpStatus.BAD_REQUEST);
+
+    return operations;
+  }
+});
--- test/utils/generate-data.ts
+++ test/utils/generate-data.ts
@@ -3,12 +3,18 @@ import { Connection } from 'typeorm';
 import { AccountService } from '../../src/entities-manager/account/account.service';
 import { CreateAccountDto } from '../../src/entities-manager/account/dto/create-account.dto';
 import { Account } from '../../src/entities-manager/account/entities/account.entity';
+import { OperationType } from '../../src/entities-manager/enums/operation-type.enum';
+import { CreateOperationDto } from '../../src/operation/dto/create-operation.dto';
+import { Operation } from '../../src/operation/entities/operation.entity';
+import { Utils } from './utils';
 
 const countRandomData = 10;
 export const accountsMock: Account[] = [];
+export const operationsMock: Operation[] = [];
 
 export async function generateData(app: INestApplication, conn: Connection) {
   await insertAccounts(app, conn);
+  await insertOperations(app, conn);
 }
 
 async function insertAccounts(app: INestApplication, conn: Connection) {
@@ -19,4 +25,22 @@ async function insertAccounts(app: INestApplication, conn: Connection) {
     accountsMock.push(accountsRepository.create(account));
   }
   await accountsRepository.save(accountsMock);
+}
+
+async function insertOperations(app: INestApplication, conn: Connection) {
+  const operationRepository = conn.getRepository(Operation);
+  for (let i = 0; i < countRandomData; i++) {
+    const operation: CreateOperationDto = {
+      amount: Utils.chance.floating({ min: 0.01, max: 9999, fixed: 2 }),
+      accountId:
+        accountsMock[Math.floor(Math.random() * accountsMock.length)].id, // random account id
+    };
+    const randomType = Utils.chance.bool()
+      ? OperationType.DEPOSIT
+      : OperationType.WITHDRAW;
+    operationsMock.push(
+      operationRepository.create({ ...operation, type: randomType }),
+    );
+  }
+  await operationRepository.save(operationsMock);
 }
